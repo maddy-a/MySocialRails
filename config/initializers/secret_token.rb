1ad8962bfaa69e322e25640e2899f2a4243bde0d
charlievil <charlievil@gmail.com>
2012-04-20 23:27:27 +0100

    InitialCommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialRails::Application.config.secret_token = 'bccc3abb0e6f9e9728e48cc5e515ab60faa6e59f2cd65334e3496b529c7328ceac89e976984116057384fe55392945382f249c72caac24767c95d034602ed655'
